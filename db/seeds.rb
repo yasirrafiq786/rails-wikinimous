13acc9b7fb3d482afb6b27ce3030858d017c5286
Yasir Malik <yasirrafiq786@gmail.com>
2020-04-26 19:45:41 +1000

    checking different forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+Article.create(title: Faker::FunnyName.name, content: 'Omnis distinctio delectus ut. Voluptatem numquam nemo recusandae voluptatem officia. Alias fugiat quos sequi ratione perferendis. Quasi totam optio sunt ea.
+
+
+
+Proactive methodical capability
+Esse molestiae ipsum ullam vero harum incidunt repellendus. Dolor et reiciendis et explicabo voluptates omnis. Architecto esse dignissimos eum eius.
+
+Rem aut quia. Neque odit vel magnam rerum fugit. Amet exercitationem incidunt et aut qui dolorem. Et consequatur aliquam nam ea quia sapiente. Placeat quo enim est totam quia nisi quibusdam.
+
+nihil Ea dolore voluptas.Ipsam quod nulla in non iste animi.Qui labore est sint voluptas fugit.
+
+Expanded logistical encoding
+Autem occaecati sunt nisi.Consequatur vel saepe enim voluptas accusantium.Recusandae id ullam adipisci voluptas ad non. Small Granite Shoes Provident veniam eius in temporibus sit iusto.Illo et soluta repellendus eius perspiciatis.Consequatur sed ullam ipsa tempora.
+
+Sit dolores ut error autem enim sed. Ipsa dolor quasi placeat maiores. Qui non reprehenderit nostrum consequatur. Eius impedit ut quo quae incidunt similique. Dolorem quibusdam autem.
+
+Et nostrum laudantium. Voluptas qui atque. Ea at et laudantium ducimus fugiat. Reiciendis aut ut hic maiores sed.
+
+Ut libero ut. Nam molestiae et laborum quae ad. Et voluptatem culpa perspiciatis eos omnis saepe. Quia quis aliquid. Molestiae facilis dolores minus unde voluptate.')
+end
